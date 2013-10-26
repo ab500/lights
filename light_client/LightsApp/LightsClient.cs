7d606473c01546efa147d6d4c5defbc909f56ebc
Andrew Robinson <andrew.robinson@gmail.com>
2013-10-26 14:50:26 -0700

    Fleshing out client interface.

--- light_client/LightsApp/LightsClient.cs
+++ light_client/LightsApp/LightsClient.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Lights
 {
+
     public class LightsClient
     {
+        [StructLayout(LayoutKind.Sequential, Pack=1)]
+        struct SettingsData
+        {
+            public byte Brightness;
+            public byte Hue;
+            public byte Saturation;
+            public byte NightMode;
+        }
+
         public enum CommandResult
         {
             Succeeded,
@@ -22,6 +33,43 @@ namespace Lights
             // else Fail and cleanup.
             await System.Threading.Tasks.Task.Delay(2000);
             return CommandResult.Succeeded;
+        }
+
+        private async Task<SettingsData> ReadSettings()
+        {
+            return new SettingsData()
+            {
+                Brightness = 200,
+                Hue = 128,
+                Saturation = 255,
+                NightMode = 0
+            };
+        }
+
+        private async Task ResetDevice()
+        {
+        }
+
+        private async Task WriteSettings(SettingsData newSettings)
+        {
+        }
+
+        public bool NightMode
+        {
+            get;
+            set;
+        }
+
+        public double Brightness
+        {
+            get;
+            set;
+        }
+
+        public double Hue
+        {
+            get;
+            set;
         }
     }
 }
