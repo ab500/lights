9bddcf91e71158ee06307f10ef43130cddc7ca57
Andrew Robinson <andrew.robinson@gmail.com>
2013-11-02 20:57:31 -0700

    Fully implemented settings read operation.

--- light_client/LightsApp/LightsClient.cs
+++ light_client/LightsApp/LightsClient.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +13,7 @@ using Windows.Storage.Streams;
 namespace Lights
 {
 
-    public class LightsClient
+    public class LightsClient : INotifyPropertyChanged
     {
 
         #region Communication Structs
@@ -37,9 +39,9 @@ namespace Lights
             Failed
         };
 
-        private static const byte ReadSettingsServiceId = 1;
-        private static const byte WriteSettingsServiceId = 2;
-        private static const string _hostPort = "5001";
+        private const byte ReadSettingsServiceId = 1;
+        private const byte WriteSettingsServiceId = 2;
+        private const string _hostPort = "5001";
         private Windows.Networking.Sockets.StreamSocket _socket = null;
         private SettingsData _currentData;
         private SettingsData? _pendingData;
@@ -57,7 +59,6 @@ namespace Lights
                 var host = new HostName(hostString);
                 await _socket.ConnectAsync(host, _hostPort);
                 await ReadSettings();
-
             }
             catch (Exception e) {
                 System.Diagnostics.Debug.WriteLine("Connection failed: " + e.Message);
@@ -76,37 +77,60 @@ namespace Lights
                 ServiceId = 1,
                 Payload = null
             };
-            byte[] commandAsBytes = CommandAsBytes(lc);
+
+            byte[] responseAsBytes = null;
+            byte[] commandAsBytes = CommandAsByteArray(lc);
 
             try
             {
                 DataWriter writer = new DataWriter(_socket.OutputStream);
+                DataReaderLoadOperation drlo = null;
+
                 writer.WriteBytes(commandAsBytes);
                 await writer.StoreAsync();
 
                 DataReader reader = new DataReader(_socket.InputStream);
-                
-                await reader.LoadAsync(4);
-                uint bytesToRead = reader.ReadUInt32();
-                
-                if (bytesToRead > 1024)
+                drlo = reader.LoadAsync(4);
+                await Task.WhenAny(new Task[] { drlo.AsTask(), Task.Delay(2000) });
+                if (drlo.Status != Windows.Foundation.AsyncStatus.Completed)
                 {
-                    System.Diagnostics.Debug.WriteLine("Very large size read requested. Closing connection.");
+                    drlo.Cancel();
+                    throw new Exception();
                 }
 
+                uint bytesToRead = reader.ReadUInt32();
+                
+                if (bytesToRead != Marshal.SizeOf<SettingsData>())
+                {
+                    System.Diagnostics.Debug.WriteLine("Incorrect byte size received.");
+                    throw new Exception();
+                }
+
+                drlo = reader.LoadAsync(bytesToRead);
+                await Task.WhenAny(new Task[] { drlo.AsTask(), Task.Delay(2000) });
+                if (drlo.Status != Windows.Foundation.AsyncStatus.Completed)
+                {
+                    drlo.Cancel();
+                    throw new Exception();
+                }
+
+                responseAsBytes = new byte[bytesToRead];
+                reader.ReadBytes(responseAsBytes);
             }
             catch (Exception e)
             {
-
+                System.Diagnostics.Debug.WriteLine("Read failed: " + e.Message);
+                CloseConnection(true);
+                return;
             }
 
-
-            // Convert LightsCommand into byte stream
-            // Send byte stream
-            // await the new response
-            // fill a byte array
-            // cast the right parts back
-
+            SettingsData sd = ByteArrayAsDataStruct<SettingsData>(responseAsBytes);
+            _hue = (double)sd.Hue / 255;
+            _brightness = (double)sd.Brightness / 255;
+            _nightMode = sd.NightMode == 1;
+            OnPropertyChanged("Hue");
+            OnPropertyChanged("Brightness");
+            OnPropertyChanged("NightMode");
         }
         private async Task WriteSettings(SettingsData newSettings)
         {
@@ -117,6 +141,22 @@ namespace Lights
         }
 
         #region Communication Helpers
+        private void CloseConnection(bool fireErrorEvent)
+        {
+            System.Diagnostics.Debug.WriteLine("Closing connection due to error.");
+            _socket.Dispose();
+            _socket = null;
+            if (fireErrorEvent)
+            {
+                var handler = ConnectionLost;
+                if (handler != null)
+                {
+                    handler.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
         struct Int32Converter
         {
             [FieldOffset(0)]
@@ -146,7 +186,7 @@ namespace Lights
                 return new Int32Converter(value);
             }
         }
-        private byte[] CommandAsBytes(LightsCommand lc)
+        private byte[] CommandAsByteArray(LightsCommand lc)
         {
             const int lengthSpecifierSize = 4;
             const int serviceIdSize = 1;
@@ -159,7 +199,6 @@ namespace Lights
             retArray[1] = serviceIdConverter.Byte2;
             retArray[2] = serviceIdConverter.Byte3;
             retArray[3] = serviceIdConverter.Byte4;
-
             retArray[4] = lc.ServiceId;
 
             if (lc.Payload != null)
@@ -169,28 +208,50 @@ namespace Lights
 
             return retArray;
         }
+
+        private T ByteArrayAsDataStruct<T>(byte[] bytes) where T : struct
+        {
+            GCHandle handle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
+            T retStruct = Marshal.PtrToStructure<T>(handle.AddrOfPinnedObject());
+            handle.Free();
+            return retStruct;
+        }
         #endregion
 
-
+        public event PropertyChangedEventHandler PropertyChanged;        
+        private void OnPropertyChanged([CallerMemberName]string caller = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(caller));
+            }
+        }
 
         #region Public Properties
+        private bool _nightMode = false;
+        private double _brightness = 0.0;
+        private double _hue = 0.0;
+
         public bool NightMode
         {
-            get;
-            set;
+            get { return _nightMode; }
+            set { _nightMode = value; OnPropertyChanged(); }
         }
 
         public double Brightness
         {
-            get;
-            set;
+            get { return _brightness; }
+            set { _brightness = value; OnPropertyChanged(); }
         }
 
         public double Hue
         {
-            get;
-            set;
+            get { return _hue; }
+            set { _hue = value; OnPropertyChanged(); }
         }
         #endregion
+
+
     }
 }
